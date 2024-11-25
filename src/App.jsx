77f33107a3f63ed9f70ccb747762d96e31933c43
akashnewport <shaliniragu53@gmail.com>
2024-11-25 17:32:20 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  
+  return (
+    <div>
+      <Header />
+      <h1>App Component</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1>Header</h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium veniam et iste nostrum, accusamus porro a facilis dolorum, repudiandae possimus, voluptate mollitia fugit reprehenderit esse. Velit saepe perferendis itaque inventore.</p>
+    </div>
+  )
+}
+
+export default Header
